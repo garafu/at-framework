1c47c49695b6ea474ca7b37d3f60c58c2f0e7d2d
Akinari Tsugo <garafo@gmail.com>
2020-09-25 13:26:41 +0900

    Add other method support.

--- net/webclient.js
+++ net/webclient.js
@@ -3,36 +3,97 @@ var https = require("https");
 var querystring = require("querystring");
 var i18n = require("iconv-lite");
 
+var getProtocol = function (protocol) {
+  switch (protocol) {
+    case "http:":
+      return http;
+    case "https:":
+      return https;
+    default:
+      throw new Error("protocol error.");
+  }
+};
+
 var WebClient = function () {
 
 };
 
 /**
  * Send http request.
- * @param   {string}    options.url       URL.
- * @param   {string}    options.method    Request method.
- * @param   {object}    options.data      Request data.
+ * @param   {string}    options.url         URL.
+ * @param   {string}    options.method      Request method.
+ * @param   {object}    options.data        Request data.
  * @param   {string}    options.contentType Request content-type.
- * @param   {string}    options.encoding  Request/respons encoding.
- * @param   {object}    options.headers   Request headers. Key-value object.
- * @param   {string}    options.username  Basic authentication username.
- * @param   {string}    options.password  Basic authentication password.
- * @param   {function}  options.error     Call back function that is called when error occured.
- * @param   {function}  options.success   Call back function that is caleed when request is success.
+ * @param   {string}    options.encoding    Request/respons encoding.
+ * @param   {object}    options.headers     Request headers. Key-value object.
+ * @param   {string}    options.username    Basic authentication username.
+ * @param   {string}    options.password    Basic authentication password.
+ * @param   {function}  options.error       Call back function that is called when error occured.
+ * @param   {function}  options.success     Call back function that is caleed when request is success.
  */
 WebClient.request = function (options) {
   switch ((options.method || "GET").toUpperCase()) {
     case "GET":
-      return WebClient.get(options);
+    case "HEAD":
+    case "DELETE":
+    case "CONNECT":
+    case "OPTIONS":
+    case "TRACE":
+      return WebClient.read(options);
     default:
-      return WebClient.post(options);
+      return WebClient.write(options);
   }
 };
 
+WebClient.get = function (optioins) {
+  optioins.method = "GET";
+  return WebClient.read(optioins);
+};
+
+WebClient.head = function (optioins) {
+  optioins.method = "HEAD";
+  return WebClient.read(optioins);
+};
+
+WebClient.post = function (optioins) {
+  optioins.method = "POST";
+  return WebClient.write(optioins);
+};
+
+WebClient.put = function (optioins) {
+  optioins.method = "PUT";
+  return WebClient.write(optioins);
+};
+
+WebClient.delete = function (optioins) {
+  optioins.method = "DELETE";
+  return WebClient.read(optioins);
+};
+
+WebClient.connect = function (optioins) {
+  optioins.method = "CONNECT";
+  return WebClient.read(optioins);
+};
+
+WebClient.options = function (options) {
+  optioins.method = "OPTIONS";
+  return WebClient.read(optioins);
+};
+
+WebClient.trace = function (options) {
+  optioins.method = "TRACE";
+  return WebClient.read(optioins);
+};
+
+WebClient.patch = function (options) {
+  optioins.method = "PATCH";
+  return WebClient.write(optioins);
+};
+
 /**
- * Send "GET" request.
+ * Send "GET", "HEAD", "DELETE", "CONNECT", "OPTIONS, ""TRACE"  request.
  */
-WebClient.get = function (options) {
+WebClient.read = function (options) {
   var url, search, req, protocol;
 
   // Create request URL.
@@ -49,23 +110,14 @@ WebClient.get = function (options) {
   }
 
   // Get protocol.
-  switch (url.protocol) {
-    case "http:":
-      protocol = http;
-      break;
-    case "https:":
-      protocol = https;
-      break;
-    default:
-      throw new Error("protocol error.");
-  }
+  protocol = getProtocol(url.protocol);
 
   // Create and execute request.
   req = protocol.request({
     protocol: url.protocol || "http:",
     host: url.hostname || "localhost",
     port: url.port,
-    method: "GET",
+    method: options.method || "GET",
     path: (url.pathname + search),
     headers: options.headers,
     auth: (options.username && optioins.password) ? `${options.username}:${options.password}` : undefined
@@ -80,25 +132,16 @@ WebClient.get = function (options) {
 };
 
 /**
- * Send "POST" request.
+ * Send "POST", "PUT", "PATCH" request.
  */
-WebClient.post = function (options) {
+WebClient.write = function (options) {
   var url, body, headers, contentType, req, protocol;
 
   // Create request URL.
   url = require("url").parse(options.url);
 
   // Get protocol.
-  switch (url.protocol) {
-    case "http:":
-      protocol = http;
-      break;
-    case "https:":
-      protocol = https;
-      break;
-    default:
-      throw new Error("protocol error.");
-  }
+  protocol = getProtocol(url.protocol);
 
   // Get content-type.
   headers = options.headers || {};
